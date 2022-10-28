99684c757612dc259df291175599f1a75839aa7e
Will <wmchance@gmail.com>
2022-10-28 13:08:33 +0700

    Add 'userId' foreignKey

--- models/course.js
+++ models/course.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Course.belongsTo(models.User);
+      Course.belongsTo(models.User, { foreignKey: 'userId' });
     }
   }
   Course.init({
